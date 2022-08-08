876d442e16771c393fd34a680a5a24cd1d0a24ad
MazzCode <mazzcode@gmail.com>
2022-08-07 23:26:20 -0300

    nuevo metodo y vista

--- Controllers/AmigosController.cs
+++ Controllers/AmigosController.cs
@@ -67,5 +67,52 @@ namespace InstaMazz2._0.Controllers
 
             }
         }
+
+        public ActionResult ListaAmigosVista()
+        {
+            ViewBag.listaSolicitudes = ListaAmigos().ToList();
+            return View();
+            //return View(Listar());
+        }
+
+        public List<Amigos> ListaAmigos()
+        {
+            List<Amigos> _lista = new List<Amigos>();
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_ListaAmigos", cn);
+                /* cmd.Parameters.AddWithValue("idEmail", Session["usuario"]);*/
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Amigos oLista = new Amigos();
+
+                        //agregamos al objeto...
+                        oLista.Id = (int)dr["Id"];
+                        oLista.IdUsu = (int)dr["IdUsu"];
+                        oLista.IdUsuAmigo = (int)dr["IdUsuAmigo"];
+                        oLista.Activo = (int)dr["Activo"];
+
+                        //oLista.UserName = dr["UserName"].ToString();
+
+                        //agregamos a la lista el objeto de list...
+                        _lista.Add(oLista);
+
+
+                    }
+                }
+                //cerramos la Conexión...
+                cn.Close();
+
+                //retornamos la lista...
+                return _lista;
+
+            }
+        }
     }
 }
