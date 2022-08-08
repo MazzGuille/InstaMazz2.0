b046b8b1d297e7214dd2855fbdd4f3baa7574535
MazzCode <mazzcode@gmail.com>
2022-08-08 00:27:28 -0300

    btn me gusta

---
+++ Content/JS/MeGusta.js
@@ -0,0 +1,9 @@
+﻿const btnElm = document.getElementById('btn-click');
+const pElm = document.getElementById('contar-click');
+var contar = 0;
+pElm.textContent = 0;
+
+btnElm.onclick = function () {
+    contar++;
+    pElm.innerHTML = contar;
+}
