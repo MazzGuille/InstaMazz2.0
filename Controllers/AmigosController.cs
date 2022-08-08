a5b659e2fe384f1bfafcf393b7f5d37b2622a167
MazzCode <mazzcode@gmail.com>
2022-08-07 22:51:05 -0300

    Nuevo controlador y clase
    Se agrego el controlador para amigos y el modelo

---
+++ Controllers/AmigosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstaMazz2._0.Controllers
+{
+    public class AmigosController : Controller
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["InstaMaczzDB"].ConnectionString;
+        // GET: Amigos
+        public ActionResult AgregarAmigo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Amigos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaMazz2._0.Models
+{
+    public class Amigos
+    {
+        public int Id { get; set; }
+        public int IdUsu { get; set; }
+        public int IdUsuAmigo { get; set; }
+        public int Activo { get; set; }
+    }
+}
