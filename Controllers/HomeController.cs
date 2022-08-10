89bc82b7a6aab7109d752198b86c893b9c4f129c
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-08-10 18:09:52 -0300

    ok
    ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -84,7 +84,6 @@ namespace InstaMazz2._0.Controllers
                     _serAmigo = false;
                 }
                 //pasamos si es fue aceptado o no la solicitud...
-                //ViewBag.Amigos = _serAmigo;
             }
             else
             {
@@ -99,6 +98,7 @@ namespace InstaMazz2._0.Controllers
                 _idUsuAmigo = 0;
                 _serAmigo = false;
             }
+            //Vemos las Publicaciones del Usuario... Segun sea el EMAIL que le pasemos...
             ViewBag.Publicaciones = ListaPublicaiones(idE);
             ViewBag.usu = _usu;
             //pasamos si es fue aceptado o no la solicitud...
@@ -184,8 +184,7 @@ namespace InstaMazz2._0.Controllers
             {
                 cn.Open();
                 SqlCommand cmd = new SqlCommand("sp_Obtener", cn);
-                cmd.Parameters.AddWithValue("idEmail", idE);//Session["usuario"]
-
+                cmd.Parameters.AddWithValue("idEmail", idE);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 using (var dr = cmd.ExecuteReader())
@@ -210,25 +209,6 @@ namespace InstaMazz2._0.Controllers
             }
             //return oLista;
         }
-
-        //public ActionResult converetImagen(string correoId)
-        //{
-        //    //cadena de conexión...
-        //    using (SqlConnection cn = new SqlConnection(cadena))
-        //    {
-        //        var cmd = new SqlCommand("SP_GET_ImgPerfil", cn);
-        //        cmd.Parameters.AddWithValue("emailId", correoId);
-        //        cmd.CommandType = CommandType.StoredProcedure;
-        //        cn.Open();
-        //        using (SqlDataReader dr = cmd.ExecuteReader())
-        //        {
-        //            byte[] _byteImg = (byte[])dr["ImagenPerfil"];
-        //            var _byteString = System.Text.Encoding.Default.GetString(_byteImg);
-
-        //            return File(_byteString, "imagenes/jpg");
-        //        }
-        //    }
-        //}
 
         public ActionResult BuscarView()
         {
