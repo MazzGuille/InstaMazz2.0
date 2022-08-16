756f34ab7e0017c9880c7d77c7a2ec85cdfdb1f7
MazzCode <mazzcode@gmail.com>
2022-08-16 17:10:56 -0300

    front changes

--- Content/JS/Input.js
+++ Content/JS/Input.js
@@ -1,2 +1,15 @@
-﻿
+﻿var boton = document.getElementById('Boton');
+var fileName = document.getElementById('fileName')
+var imagen = document.getElementById('imagen')
 
+boton.onchange = (e) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(boton.files[0]);
+    console.log(boton.files[0]);
+    reader.onload = () => {
+        imagen.setAttribute("src", reader.result)
+    }
+    reader.onload = () => {
+        fileName.textContent = fileName.textContent + boton.files[0].name;
+    }
+}
