653262b59c6175f8c0da5b2e3928a605f729d85b
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-08-18 14:38:18 -0300

    Vista
    se genero la lista de chats...

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -19,16 +19,50 @@ namespace InstaMazz2._0.Controllers
         // GET: Chat
         public ActionResult Chat()
         {
+            //enviamos la charla del usuario con sus amigos...
+            //--- a la vista...
+            ViewBag.Chats = GetChats().ToList();
             return View();
         }
 
-        private static List<MChats> GetChats()
+        private List<MChats> GetChats()
         {
-            List<MChats> mChats = new List<MChats>();
+            List<MChats> _mChats = new List<MChats>();
             string _sessionEmail = (string)Session["usuario"];
 
-            using (SqlConnection cn = new SqlConnection(cadena)) { }
+            using (SqlConnection cn = new SqlConnection(cadena)) 
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", cn);
+                cmd.Parameters.AddWithValue("idEmail", _sessionEmail);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Obtenemos todo la combersaciones del usuario con sus amigos...
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        MChats oChat = new MChats();
+
+                        //Guardamos en la lista del Modelo...
+                        oChat.ID = (int)dr["ID"];
+                        oChat.Mensaje = (string)dr["Mensaje"];
+                        oChat.Fecha = (string)dr["Fecha"];
+
+                        //Ahora lo Guardamos en la Lista del Chat...
+                        _mChats.Add(oChat);
+                    }
+                }
+
+                //Retornamos la lista del chat del usuario...
+                return _mChats;
+            }
         }
+
+        //private bool Guardar(string emailAmigo, string mensaje)
+        //{
+
+        //}
 
     }
 }
