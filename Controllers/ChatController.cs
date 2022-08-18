fed492eb119f330b9d520e4237b7513326591908
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-08-18 15:24:59 -0300

    oks
    oks

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -23,12 +23,16 @@ namespace InstaMazz2._0.Controllers
             //enviamos la charla del usuario con sus amigos...
             //--- a la vista...
             ViewBag.Chats = GetChats().ToList();
+            //lista de Amigos...
+            ViewBag.listaSolicitudes = ListaAmigos(sessionUsuario()).ToList();
             return View();
         }
 
         private List<MChats> GetChats()
         {
             List<MChats> _mChats = new List<MChats>();
+
+            //Obtenemos la session del usuario...
             string _sessionEmail = sessionUsuario();
 
             using (SqlConnection cn = new SqlConnection(cadena)) 
@@ -85,6 +89,63 @@ namespace InstaMazz2._0.Controllers
                 {
                     return false;
                 }
+            }
+        }
+
+        private List<Amigos> ListaAmigos(string IdEmail)
+        {
+            List<Amigos> _lista = new List<Amigos>();
+            // esta variable es para pasar el nombre de la imagen obtenida desde la bd...
+            string _byteString;
+
+            //esta variable cirbe para determinar, si el usuario tiene una foto de perfil o no...
+            bool _ceroImg;
+
+            using (SqlConnection cn = new SqlConnection(cadena))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_ListaAmigos", cn);
+                cmd.Parameters.AddWithValue("email", IdEmail); //Session["usuario"]
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        byte[] _byteImg = (byte[])dr["ImagenPerfil"];
+                        if (BitConverter.ToInt32(_byteImg, 0) > 0)
+                        {
+                            _byteString = System.Text.Encoding.Default.GetString(_byteImg);
+                            _ceroImg = true;
+                        }
+                        else
+                        {
+                            _byteString = "avatar.jpg";
+                            _ceroImg = false;
+                        }
+                        Amigos oLista = new Amigos();
+
+                        //agregamos al objeto...
+                        oLista.Id = (int)dr["Id"];
+                        //oLista.IdUsu = (int)dr["IdUsu"];
+                        oLista.IdUsuAmigo = (int)dr["IdUsuAmigo"];
+                        oLista.Activo = (int)dr["Activo"];
+                        oLista.Nombre = dr["Nombre"].ToString();
+                        oLista.UserName = dr["UserName"].ToString();
+                        oLista.imagenPerf = _byteString;
+                        oLista.Email = dr["Email"].ToString();
+                        oLista.ceroImg = _ceroImg;
+
+                        //agregamos a la lista el objeto de list...
+                        _lista.Add(oLista);
+                    }
+                }
+                //cerramos la Conexión...
+                cn.Close();
+
+                //retornamos la lista...
+                return _lista;
+
             }
         }
 
