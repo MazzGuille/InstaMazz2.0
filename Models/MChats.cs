f474a4b852c59083bef4d9e59af323de60fb67e5
Lucas Paz <47407604+lucaspaz89@users.noreply.github.com>
2022-08-18 14:07:00 -0300

    Modelo
    Se crearon los modelo de Chats y Post-Comentario....

---
+++ Models/MChats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaMazz2._0.Models
+{
+    public class MChats
+    {
+        public int ID { get; set; }
+        public string Mensaje { get; set; }
+        public string Fecha { get; set; }
+    }
+}
---
+++ Models/MPostComent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaMazz2._0.Models
+{
+    public class MPostComent
+    {
+        public int IdPost { get; set; }
+        public string Coment { get; set; }
+        public string Fecha { get; set; }
+        public bool Activo { get; set; }
+    }
+}
